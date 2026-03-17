333a34f3bab2542f82cdfbd91e7c291f99e583ee
jesusharo <jharolozano@gmail.com>
2026-03-16 21:35:52 -0600

    Renombrando imágenes y corrigiendo rutas finales

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Switch, Route } from 'wouter';
 import { LanguageProvider } from './lib/LanguageContext';
 import IndexPage from './pages/index'; // La página principal, antes home.tsx
-import LoginPage from './pages/login';
+import LoginPage from './pages/Login';
 import InventionemPage from './pages/Inventionem';
 import FrescosPage from './pages/Frescos';
 
